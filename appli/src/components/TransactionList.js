079770f0bf0e59c3c9a4bf00f073f5aabb4d7236
Kento-Suzuki-0706 <kento.7611@gmail.com>
2024-01-13 17:18:37 +1000

    update

--- appli/src/components/TransactionList.js
+++ appli/src/components/TransactionList.js
@@ -1,18 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const TransactionItem = styled.li`
+  background-color: white;
+  list-style-type: none;
+  padding: 10px;
+  margin-bottom: 5px;
+  border-radius: 5px;
+  color: ${props => props.type === 'expense' ? 'black' : 'green'};
+`;
+
+const DeleteButton = styled.button`
+  margin-left: 10px;
+  background-color: #FF8856;
+  color: black;
+  font-weight: bold;
+`;
 
 function TransactionList({ transactions, onDelete }) {
+  // 日付を dd/mm/yyyy 形式にフォーマットする関数
+  const formatDate = (dateString) => {
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    return new Date(dateString).toLocaleDateString('en-GB', options);
+  };
+
   return (
     <ul>
-      {transactions.map((transaction, index) => (
-        <li key={transaction.id || index}>
-          {transaction.date} - {transaction.category}: {transaction.amount} ({transaction.type})
-          <button onClick={() => onDelete(transaction.id)}>Delete</button>
-        </li>
+      {transactions.map((transaction) => (
+        <TransactionItem key={transaction.id} type={transaction.type}>
+          {formatDate(transaction.date)} - {transaction.category}: {transaction.amount} ({transaction.type})
+          <DeleteButton onClick={() => onDelete(transaction.id)}>Delete</DeleteButton>
+        </TransactionItem>
       ))}
     </ul>
   );
 }
 
 export default TransactionList;
+
 
