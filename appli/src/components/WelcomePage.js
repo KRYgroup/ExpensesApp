592694d77c6adb8cb10d0ac9b924059ebdb35d34
Kento-Suzuki-0706 <kento.7611@gmail.com>
2024-01-14 20:01:01 +1000

    update

--- appli/src/components/WelcomePage.js
+++ appli/src/components/WelcomePage.js
@@ -1,26 +1,53 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import backgroundImage1 from "../images/signup.png";
 
 const WelcomeContainer = styled.div`
-  text-align: center;
-  padding: 50px;
+  background-image: url(${backgroundImage1});
+  background-size: cover;
+  background-position: center center;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: 50%; 
+  padding-bottom: 10%; 
 `;
 
 const WelcomeTitle = styled.h1`
+  color: white;
   margin-bottom: 20px;
+  text-align: center;
+  font-size: 60px;
+`;
+
+const StyledParagraph = styled.p`
+  color: #f3f3f3; // 明るいグレーで自然な色合い
+  font-size: 20px;
+  margin-bottom: 10px; // 適切な間隔
+`;
+
+const StyledLink = styled(Link)`
+  color: #00AA00; // ソフトな青色
+  text-decoration: none; // アンダーラインなし
+
+  &:hover {
+    color: #00CC00; // ホバー時の色
+    text-decoration: underline; // ホバー時にアンダーラインを表示
+  }
 `;
 
 const WelcomePage = () => {
   return (
     <WelcomeContainer>
       <WelcomeTitle>Welcome to ExpensesApp</WelcomeTitle>
-      <p>
-        <Link to="/login">Login</Link> if you already have an account.
-      </p>
-      <p>
-        Or <Link to="/signup">Signup</Link> if you're new here.
-      </p>
+      <StyledParagraph>
+        <StyledLink to="/login">Login</StyledLink> if you already have an account.
+      </StyledParagraph>
+      <StyledParagraph>
+        Or <StyledLink to="/signup">Signup</StyledLink> if you're new here.
+      </StyledParagraph>
     </WelcomeContainer>
   );
 };
