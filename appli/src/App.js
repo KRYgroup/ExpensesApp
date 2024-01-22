bde5f3a29bec511311925f2ae7f90eb04a4f67ef
YosukeKono0101 <n11230380@qut.edu.au>
2024-01-22 15:57:26 +1000

    Update

--- appli/src/App.js
+++ appli/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
-import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Signup from "./components/Signup";
